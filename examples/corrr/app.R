639c0d0f622fb75b2b1e52b7f85f24846b3644ef
Richard Iannone <riannone@me.com>
2020-07-24 17:06:28 -0400

    Improve appearance of correlation plot extension

--- examples/corrr/app.R
+++ examples/corrr/app.R
@@ -1,26 +1,13 @@
-options(shiny.port = 2468)
+# filetype: shinyApp
 
 library(shiny)
 library(shinytableau)
+library(thematic)
 library(corrr)
 library(ggplot2)
 library(promises)
 
-# TODO: yaml file?
-manifest <- tableau_manifest(
-  #source_location = "https://jcheng.shinyapps.io/tableautest/?mode=embed",
-  extension_id = "com.example.corrr",
-  extension_version = "1.1.3",
-  name = "corrr",
-  description = "Create a correlation plot based on a single dataset",
-  extended_description = tagList(
-    tags$p("This is an extension for Tableau dashboards that creates a correlation matrix visualization.")
-  ),
-  author_name = "Jane Doe",
-  author_email = "jane_doe@example.com",
-  author_organization = "Example Corp.",
-  website = "https://example.com/tableau/extensions/corrr"
-)
+manifest <- tableau_manifest_from_yaml("manifest.yml")
 
 ui <- function(req) {
   fillPage(
@@ -37,17 +24,90 @@ server <- function(input, output, session) {
 
     df() %...>% {
 
-      numeric_cols <- vapply(., FUN.VALUE = logical(1), FUN = is.numeric)
+      get_r_column_names_types <- function(tbl) {
 
-      corrr::rplot(corrr::correlate(x = .[, numeric_cols, drop = FALSE], quiet = TRUE)) +
-        ggplot2::theme(
-          axis.text.x = ggplot2::element_text(
-            angle = 90, vjust = 0.5, hjust = 1
-          ),
-          text = ggplot2::element_text(
-            size = 16, face = "bold"
-          )
+        suppressWarnings(
+          column_names_types <-
+            tbl %>%
+            utils::head(1) %>%
+            dplyr::collect() %>%
+            vapply(
+              FUN.VALUE = character(1),
+              FUN = function(x) class(x)[1]
+            )
         )
+
+        list(
+          col_names = names(column_names_types),
+          r_col_types = unname(unlist(column_names_types))
+        )
+      }
+
+      get_corr_plot <- function(mat,
+                                labels_vec) {
+
+        corr_df <-
+          as.data.frame(as.table(mat)) %>%
+          dplyr::mutate(Freq = ifelse(Var1 == Var2, NA_real_, Freq)) %>%
+          dplyr::mutate(Var1 = factor(Var1, levels = names(labels_vec))) %>%
+          dplyr::mutate(Var2 = factor(Var2, levels = rev(names(labels_vec))))
+
+
+        corr_df %>%
+          ggplot2::ggplot(ggplot2::aes(x = Var1, y = Var2, fill = Freq)) +
+          ggplot2::geom_tile(color = "white", linejoin = "bevel") +
+          ggplot2::scale_fill_gradientn(
+            colours = c("blue", "white", "red"),
+            na.value = "gray30",
+            limits = c(-1, 1)
+          ) +
+          ggplot2::labs(x = "", y = "") +
+          ggplot2::theme_minimal() +
+          ggplot2::theme(
+            axis.text.x = ggplot2::element_text(
+              angle = 90, vjust = 0.5, hjust = 1, size = 10
+            ),
+            axis.text.y = ggplot2::element_text(size = 10),
+            panel.grid = ggplot2::element_blank(),
+            legend.direction = "horizontal",
+            legend.title = ggplot2::element_blank(),
+            legend.position = c(0.5, 1.03),
+            plot.margin = ggplot2::unit(c(1, 0.5, 0, 0), "cm"),
+            legend.key.width = ggplot2::unit(2.0, "cm"),
+            legend.key.height = ggplot2::unit(3.0, "mm")
+          )
+      }
+
+      tbl_info <- get_r_column_names_types(tbl = .)
+      col_names <- tbl_info$col_names
+      col_types <- tbl_info$r_col_types
+
+      columns_numeric <- col_names[col_types %in% c("integer", "numeric")]
+      data_corr <- dplyr::select(., dplyr::one_of(columns_numeric))
+
+      corr_pearson <- stats::cor(data_corr, method = "pearson", use = "pairwise.complete.obs")
+
+      cols_to_remove <-
+        vapply(
+          colnames(corr_pearson), FUN.VALUE = logical(1), USE.NAMES = TRUE,
+          FUN = function(x) {
+
+            corr_pearson %>% dplyr::as_tibble() %>% dplyr::pull({{ x }}) %>%
+              is.na() %>% all()
+          }
+        ) %>% which() %>% names()
+
+      if (length(cols_to_remove) > 0) {
+
+        data_corr <- data_corr %>% select(-{{ cols_to_remove }})
+        corr_pearson <- stats::cor(data_corr, method = "pearson", use = "pairwise.complete.obs")
+      }
+
+      labels_vec <- seq_along(colnames(corr_pearson))
+      names(labels_vec) <- colnames(corr_pearson)
+
+      corr_plot <- get_corr_plot(mat = corr_pearson, labels_vec = labels_vec)
+      corr_plot
     }
   })
 }
@@ -97,4 +157,7 @@ config_server <- function(input, output, session) {
   })
 }
 
-tableau_extension(manifest, ui, server, config_ui, config_server)
+tableau_extension(
+  manifest, ui, server, config_ui, config_server,
+  options = list(port = 2468)
+)
