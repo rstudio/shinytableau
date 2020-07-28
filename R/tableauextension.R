c4fc5de76ba49301c00f409d87119c216a35da91
Richard Iannone <riannone@me.com>
2020-07-28 11:09:40 -0400

    Use `thematic_shiny()` in `tableau_extension()`

--- R/tableauextension.R
+++ R/tableauextension.R
@@ -2,6 +2,13 @@
 tableau_extension <- function(manifest, ui, server, config_ui = NULL,
   config_server = NULL, options = list()) {
 
+  # Here `thematic::shiny()` is used to specify a font replacement;
+  # The 'Open Sans' font will be obtained from Google Fonts (i.e.,
+  # downloaded and installed on the user's system) if not already
+  # available; after exiting from the `tableau_extension()` Shiny app,
+  # thematic will clean up after itself (font option is used globally)
+  thematic::thematic_shiny(font = "Open Sans")
+
   shiny::shinyApp(
     tableau_ui(manifest, ui, config_ui),
     tableau_server(server, config_server),
