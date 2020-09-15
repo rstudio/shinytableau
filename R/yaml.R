86ff81007176538a70992df050ce5a5e90a89302
Joe Cheng <joe@rstudio.com>
2020-09-15 09:52:18 -0700

    yaml file documentation

--- R/yaml.R
+++ R/yaml.R
@@ -1,25 +1,48 @@
-#' Create a skeleton YAML manifest file
+#' Create and use a skeleton YAML manifest file
 #'
-#' Tableau extensions require a manifest and the use of a YAML manifest file in
-#' **shinytableau** allows for a separation of concerns when developing an
-#' extension. With the `yaml_skeleton()` function, we can write a YAML manifest
-#' file to disk; it contains all of the required fields for the extension with
-#' example values. Later in the development lifecycle, the developer of a
-#' **shinytableau** extension can use values relevant to the project.
+#' Tableau extensions need to provide a
+#' [manifest](https://tableau.github.io/extensions-api/docs/trex_manifest.html)
+#' (`.trex`) file that Tableau users can load into their dashboards. This file
+#' contains metadata such as the ID and name of the extension, the name and
+#' email of the extension author, logo image, etc. With shinytableau, you as the
+#' extension author do not create this file directly; instead, you provide a
+#' simpler manifest.yml file.
 #'
-#' @param filename A filename for the YAML manifest file. Can be combined with a
-#'   relative path. The default, and recommended, filename is `"manifest.yml"`.
+#' Call `yaml_skeleton()` from the R console to create a manifest.yml file.
+#'
+#' Call `tableau_manifest_from_yaml()` from your app.R to load it, and pass the
+#' resulting object to [tableau_extension()].
+#'
+#' @param filename The (relative or absolute) file path where the YAML manifest
+#'   file should be written. For an app.R based app, the recommended file path
+#'   is `manifest.yml` in the same directory as app.R; for a package-based app,
+#'   put the file in the package's `inst` directory, and call
+#'   `tableau_manifest_from_yaml()` with [system.file()].
+#' @param path The (relative or absolute) file or directory path from which to
+#'   load an existing YAML manifest file. If the path is a directory, the
+#'   filename `manifest.yml` will be added to the end of the path.
+#' @return `yaml_skeleton` returns the filename, invisibly.
+#'   `tableau_manifest_from_yaml` returns a manifest object that is suitable for
+#'   passing to [tableau_extension()].
+#'
+#' @seealso See the [Getting Started
+#'   guide](https://rstudio.github.io/shinytableau/articles/shinytableau.html)
+#'   to learn more.
+#'
+#'   If the YAML file approach is inconvenient for some reason, you can also use
+#'   [tableau_manifest()] to programmatically create a manifest.
 #'
 #' @examples
+#' \dontrun{
 #' # Generate a YAML manifest file in the
 #' # root directory of the project
-#' # yaml_skeleton()
+#' yaml_skeleton()
 #'
 #' # Generate the skeleton YAML manifest
 #' # file (called `manifest.yml`) in the
 #' # `app` subdirectory
-#' # yaml_skeleton(filename = "app/manifest.yml")
-#'
+#' yaml_skeleton(filename = "app/manifest.yml")
+#' }
 #' @export
 yaml_skeleton <- function(filename = "manifest.yml") {
 
@@ -59,6 +82,7 @@ yaml_skeleton <- function(filename = "manifest.yml") {
   invisible(filename)
 }
 
+#' @rdname yaml_skeleton
 #' @export
 tableau_manifest_from_yaml <- function(path = ".") {
 
